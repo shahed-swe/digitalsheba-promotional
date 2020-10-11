d1a093942df831d4f2a901a91c80df75a748ee40
Md Shahed Talukder <shahedtalukder51@gmail.com>
2020-10-11 15:32:04 +0600

    Updates

---
+++ precache-manifest.ff59d131dc6e624a87f0eceb143acd2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d972dd5e68dc31fc830f97751fd76c23",
+    "url": "/digitalsheba-promotional/index.html"
+  },
+  {
+    "revision": "d4b12b130eca723f592e",
+    "url": "/digitalsheba-promotional/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d0b8d2bb5438d4ebaf4",
+    "url": "/digitalsheba-promotional/static/js/2.4756d538.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/digitalsheba-promotional/static/js/2.4756d538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b12b130eca723f592e",
+    "url": "/digitalsheba-promotional/static/js/main.52157e46.chunk.js"
+  },
+  {
+    "revision": "9941b8df1ccb517661a4",
+    "url": "/digitalsheba-promotional/static/js/runtime-main.906c70d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/digitalsheba-promotional/static/media/logo.5d5d9eef.svg"
+  }
+]);
